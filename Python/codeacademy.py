06d6bb75dd19b357a4f28d6f225a398fafb657ed
Sammygun <sam.rybtsov@gmail.com>
2021-01-14 17:58:41 +0300

    Django_конспект, codeacademy

---
+++ Python/codeacademy.py
@@ -0,0 +1,22 @@
+												Содержание 
+
+												_calculations_
+												_exponents_
+												_
+===================================================
+ _calculations_
+
+1 print(25 * 68 + 13 / 28) # calculation in print
+# 1700.4642857142858
+===================================================
+_exponents_
+1
+# 2 to the 10th power, or 1024
+print(2 ** 10)
+ 
+# 8 squared, or 64
+print(8 ** 2)
+ 
+# 9 * 9 * 9, 9 cubed, or 729
+print(9 ** 3)
+
